13a1bd62c9fd30fb929fbb057df0e6e6d9863ac5
Mohamed <mokasha810@gmail.com>
2022-07-24 15:07:29 +0200

    add backend unit testing

--- front-end/src/components/Ip/__test__/IpSection.test.js
+++ front-end/src/components/Ip/__test__/IpSection.test.js
@@ -1,7 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import geoIpifyMock from "../../../mocks/geoIpify.json";
 import IpSection from "../IpSection";
 
 describe("testing ip address section", () => {
@@ -31,3 +33,33 @@ describe("testing ip address section", () => {
     expect(screen.queryByText(/Enter a valid IP Address/i)).toBeInTheDocument();
   });
 });
+
+const server = setupServer(
+  rest.get(
+    "https://geo.ipify.org/api/v2/country?apiKey=at_OL5AP2QeUTdWsdO9rqHUKW91v8NLc&ipAddress=8.8.8.8",
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(geoIpifyMock));
+    }
+  )
+);
+
+// describe("test Ipify Api", () => {
+// beforeAll((done) => {
+//   server.listen();
+//   done();
+// });
+
+// afterAll(() => {
+//   server.close();
+// });
+
+// afterEach(() => {
+//   server.resetHandlers();
+// });
+
+// test("test", async () => {
+//   render(<IpSection />);
+//   const elem = await screen.findByTestId("ip");
+//   expect(elem.innerText).toBe("8.8.8.8");
+// });
+// });
